8fccec0fe7e9f8069981c42a9300f5404c33f011
hy1263 <1263038897@qq.com>
2024-05-13 10:05:55 +0800

    Initial commit

---
+++ src/a.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: a
+ * Description:
+ *
+ * @Author hy
+ * @Create 13/5/2024 上午10:03
+ * @Version 1.0
+ */
+public class a {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
